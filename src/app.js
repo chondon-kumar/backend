413adc7b47b6e825a452abb2d0f97b3a8e66856c
chondon_kumar <chondon914@gmail.com>
2025-03-28 19:17:43 +0600

    debug some type missing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import exopress from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
-const app = exopress()
+const app = express()
 
 app.use(cors({
     origin : process.env.CORS_ORIGIN ,
@@ -10,7 +10,7 @@ app.use(cors({
 
 app.use(express.json({limit:"16kd"})) ;// don`t over-incress limit unless you have a spacific reson 
 app.use(express.urlencoded({extended : true , limit : "16kd"})) ; // urlencoded use for url limiting 
-app.use(exopress.static("public"));
+app.use(express.static("public"));
 app.use(cookieParser())
 
 export { app }
